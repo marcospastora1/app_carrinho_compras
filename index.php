d7a35cc7492aa8d6550212c2eb4eae2bf3d6cc01
Marcos <marcos.santos@codenapp.com>
2022-09-21 17:44:04 -0300

    aula 6

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <?php
 
-use App\CarrinhoCompra;
-
 require __DIR__ . "/vendor/autoload.php";
 
-echo CarrinhoCompra::teste();
+
+use App\CarrinhoCompra;
+
+$carrinho1 = new CarrinhoCompra();
+
+print_r($carrinho1->exibirItens());
+echo 'Valor Total: ' . $carrinho1->exibirValorTotal();
+
+/*
+$carrinho1->adicionarItens('Bicicleta', 750.10);
+$carrinho1->adicionarItens('Geladeira', 1950.15);
+$carrinho1->adicionarItens('Tapete', 350.20); 
+*/
+
+$carrinho1->adicionarItens('TV', 2500);
+
+echo '<br/>';
+print_r($carrinho1->exibirItens());
+echo 'Valor total recalculado: ' . $carrinho1->exibirValorTotal();
+
+echo '<br/>';
+echo 'status: ' . $carrinho1->exibirStatus();
+
+echo '<br/>';
+if ($carrinho1->confirmarPedido()) {
+  echo 'Pedido realizado com sucesso';
+} else {
+  echo 'Erro na confirmação do pedido. Carrinho não possui itens';
+}
+
+echo '<br/>';
+echo 'status: ' . $carrinho1->exibirStatus();
--- src/CarrinhoCompra.php
+++ src/CarrinhoCompra.php
@@ -4,8 +4,60 @@ namespace App;
 
 class CarrinhoCompra
 {
-  public static function teste()
+  //atributos
+  private $itens;
+  private $status;
+  private $valorTotal;
+
+  //métodos
+  public function __construct()
   {
-    return 'ok';
+    $this->itens = [];
+    $this->status = 'Aberto';
+    $this->valorTotal = 0;
+  }
+
+  public function exibirItens()
+  {
+    return $this->itens;
+  }
+
+  public function adicionarItens(string $item, float $valor)
+  {
+    array_push($this->itens, ['item' => $item, 'valor' => $valor]);
+    $this->valorTotal += $valor;
+    return true;
+  }
+
+  public function exibirValorTotal()
+  {
+    return $this->valorTotal;
+  }
+
+  public function exibirStatus()
+  {
+    return $this->status;
+  }
+
+  public function confirmarPedido()
+  {
+    if ($this->validarCarrinho()) {
+      $this->status = 'Confirmado';
+      $this->enviarEmailConfirmacao();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  public function enviarEmailConfirmacao()
+  {
+    echo '<br/>.......envia email de confirmação.....<br/>';
+  }
+
+  public function validarCarrinho()
+  {
+    return count($this->itens) > 0;
   }
 }
