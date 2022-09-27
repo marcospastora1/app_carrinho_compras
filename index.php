9184ed829434839de8bfffc64314125584f5555d
Marcos <marcos.santos@codenapp.com>
2022-09-27 16:10:16 -0300

    aula 10

--- index.php
+++ index.php
@@ -4,10 +4,81 @@ require __DIR__ . "/vendor/autoload.php";
 
 
 use App\CarrinhoCompra;
-
-$carrinho1 = new CarrinhoCompra();
+use App\Item;
+use App\Pedido;
+use App\EmailService;
 
 echo '<h3>Com SRP</h3>';
+
+$pedido = new Pedido();
+//--------------------------------
+$item1 = new Item();
+$item2 = new Item();
+
+$item1->setDescricao('Porta Copo');
+$item1->setValor(4.55);
+
+$item2->setDescricao('Lampada');
+$item2->setValor(8.32);
+//--------------------------------
+
+echo '<h4>Pedido Iniciado</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
+
+//--------------------------------
+$pedido->getCarrinhoCompra()->adicionarItem($item1);
+$pedido->getCarrinhoCompra()->adicionarItem($item2);
+//--------------------------------
+
+echo '<h4>Pedido com Itens</h4>';
+echo '<pre>';
+print_r($pedido);
+echo '</pre>';
+
+//--------------------------------
+
+echo '<h4>Itens do carrinho</h4>';
+echo '<pre>';
+print_r($pedido->getCarrinhoCompra()->getItens());
+echo '</pre>';
+//--------------------------------
+
+echo '<h4>Valor do pedido</h4>';
+$total = 0;
+foreach ($pedido->getCarrinhoCompra()->getItens() as $key => $item) {
+  $total += $item->getValor();
+}
+echo $total;
+
+//--------------------------------
+
+echo '<h4>Status do Pedido</h4>';
+echo ($pedido->getCarrinhoCompra()->validarCarrinho() ? 'Válido' : 'Inválido');
+
+//--------------------------------
+
+echo '<h4>Status do Pedido</h4>';
+echo $pedido->getStatus();
+
+//--------------------------------
+
+echo '<h4>Confirmar pedido</h4>';
+echo  $pedido->confirmar() ? 'Pedido foi Confirmado' : 'Pedido não confirmado';
+
+//--------------------------------
+
+echo '<h4>Status do Pedido</h4>';
+echo $pedido->getStatus();
+
+//--------------------------------
+
+echo '<h4>Email</h4>';
+if ($pedido->getStatus() == 'Confirmado') {
+  echo EmailService::dispararEmail();
+}
+
 
 /* print_r($carrinho1->exibirItens());
 echo 'Valor Total: ' . $carrinho1->exibirValorTotal();
